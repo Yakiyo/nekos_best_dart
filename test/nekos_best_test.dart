7c7ca990e3436954b67912ee028e51e8181694a7
Yakiyo <91675932+Yakiyo@users.noreply.github.com>
2022-11-04 12:07:29 +0600

    chore: rewrite tests
    - test all methods and functions

--- test/nekos_best_test.dart
+++ test/nekos_best_test.dart
@@ -3,19 +3,33 @@ import 'package:test/test.dart';
 
 void main() {
   group('nekos_best.dart test :)', () {
+    var client = nb.Client();
     test('Fetch test', () async {
       var res = await nb.fetch(endpoint: 'neko');
       expect(res.isNotEmpty, true);
+
+      var res2 = await client.fetch(endpoint: 'kitsune');
+      expect(res2.isNotEmpty, true);
     });
 
     test('Random Fetch test', () async {
-      var res = await nb.fetch();
+      var res = await client.fetch();
       expect(res.isNotEmpty, true);
     });
 
     test('Fetch Multiple test', () async {
-      var res = await nb.fetch(endpoint: 'neko', amount: 4);
+      var res = await client.fetch(endpoint: 'neko', amount: 4);
       expect(res.length == 4, true);
+    });
+
+    test('Search Test', () async {
+      var res = await client.search('Gochuumon wa Usagi Desuka??', endpoint: 'baka');
+      expect(res.isNotEmpty, true);
+    });
+
+    test('Fetch File Test', () async {
+      var res = await client.fetchFile('kitsune');
+      expect(res.data.isNotEmpty, true);
     });
   });
 }
