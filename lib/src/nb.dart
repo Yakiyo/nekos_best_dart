1a582cfd105c20f78bc8568b93efd1b4d303191c
Yakiyo <91675932+Yakiyo@users.noreply.github.com>
2022-11-29 21:18:04 +0600

    feat: handle ratelimiting for search method

--- lib/src/nb.dart
+++ lib/src/nb.dart
@@ -6,13 +6,15 @@ import 'structs.dart';
 ///
 /// If it's going to be used multiple times, it's better
 /// to initiate a [Client] class and use its fetch method
-Future<List<NBResponse>> fetch({String? endpoint = null, int amount = 1}) async {
+Future<List<NBResponse>> fetch(
+    {String? endpoint = null, int amount = 1}) async {
   return Client().fetch(endpoint: endpoint, amount: amount);
 }
 
 /// Base client to interact with the api.
 class Client {
   Map<String, dynamic> _endpoints = {};
+  RateLimitError? RateLimit;
 
   /// Getter for the endpoints
   Map<String, dynamic> get endpoint => _endpoints;
@@ -43,10 +45,20 @@ class Client {
 
   /// Search for a query
   ///
+  /// Note: This endpoint is currently unstable and may be reworked.
   /// Refer to the official docs for more
   /// https://docs.nekos.best/api/endpoints.html#get-searchqueryxtypexcategoryxamountx
   Future<List<NBResponse>> search(String query,
       {String? endpoint = null, int amount = 1}) async {
+
+    if (RateLimit is RateLimitError) {
+      if (!DateTime.now().isAfter(RateLimit!.resetsIn)) {
+        throw RateLimit as RateLimitError;
+      }
+      // RateLimit is no longer valid, so make it null
+      RateLimit = null;
+    }
+
     if (amount > 20) amount = 20;
     if (amount < 1) amount = 1;
     if (endpoint == null) {
@@ -57,12 +69,13 @@ class Client {
     }
     var type = IMAGE_CATEGORIES.contains(endpoint) ? 1 : 2;
     var res = await request(
-              "search?amount=$amount&type=$type&query=${Uri.encodeComponent(query)}&category=$endpoint");
+        "search?amount=$amount&type=$type&query=${Uri.encodeComponent(query)}&category=$endpoint");
 
     var remaining = res.headers['x-rate-limit-remaining'];
-    var resetsIn = res.headers['x-rate-limit-remaining'];
-    if (remaining != null && resetsIn != null) {
-      throw '';
+    if (remaining == 0) {
+      var resetsIn = res.headers['x-rate-limit-remaining'];
+      RateLimit = RateLimitError(remaining as String, resetsIn as String);
+      throw RateLimit as RateLimitError;
     }
     var json = jsonDecode(res.body) as Map<String, dynamic>;
     var arr = <NBResponse>[];
